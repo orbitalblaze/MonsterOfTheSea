3bbc662410deacd5d75011d4e42d8ad1818990a6
Unknown <amand5610@gmail.com>
2017-06-15 09:40:04 +0200

    Menu en jeu et début interface

---
+++ Assets/UI/Scripts/Decompte.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Decompte : MonoBehaviour
+{
+    float timeLeft = 30.0f;
+
+    public Text text;
+    
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Temps restant : " + Mathf.Round(timeLeft) + "sec";
+        if (timeLeft < 0)
+        {
+            //Application.LoadLevel("gameOver");
+            text.text = "Fin du tour";
+        }
+    }
+}

c11a9908d4776c44d28641e85592e1be3bae65a5
Luxgnite <ewan.dubois@live.fr>
2017-06-16 17:51:26 +0200

    Début Lobby connection

---
+++ Assets/Scripts/Network/UI/ListRoom.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+	public class ListRoom : MonoBehaviour {
+
+		public Lobby lobby;
+		public GameObject prefabRoomButton;
+		public List<GameObject> listRoomButton;
+	
+		void Awake () {
+			listRoomButton = new List<GameObject> ();
+		}
+
+		// Use this for initialization
+		void Start () {
+			getRooms ();	
+		}
+	
+		// Update is called once per frame
+		void Update () {
+	
+		}
+
+		public void getRooms ()
+		{
+			RoomInfo[] rooms = lobby.GetRoomList();
+			foreach (RoomInfo room in rooms) {
+				GameObject newButton = Instantiate<GameObject>(prefabRoomButton);
+				newButton.transform.parent = this.transform;
+				RoomButton infoButton = newButton.GetComponent<RoomButton>();
+				infoButton.text.text = room.Name + "(" + room.PlayerCount +"/" + room.MaxPlayers + ")";
+				infoButton.roomName = room.Name;
+				listRoomButton.Add(newButton);
+			}
+		}
+	}
---
+++ Assets/Scripts/Network/UI/RoomButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+	public class RoomButton : MonoBehaviour {
+		public string roomName;
+		public Text text;
+		public string name;
+
+		void Awake ()
+		{
+			text = GetComponentInChildren<Text> ();
+		}
+
+	}

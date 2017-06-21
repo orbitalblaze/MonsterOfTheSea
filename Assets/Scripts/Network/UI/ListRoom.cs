5dd809e1fe6132ffdbbc5ce51952081f5c3fa527
Luxgnite <ewan.dubois@live.fr>
2017-06-21 11:17:32 +0200

    Listing dynamique d'objets
    La liste est bien générée en fonction des éléments récupérés (actuellement uniquement static)

--- Assets/Scripts/Network/UI/ListRoom.cs
+++ Assets/Scripts/Network/UI/ListRoom.cs
@@ -2,36 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 
-	public class ListRoom : MonoBehaviour {
+public class ListRoom : MonoBehaviour {
 
-		public Lobby lobby;
-		public GameObject prefabRoomButton;
-		public List<GameObject> listRoomButton;
+	public Lobby lobby;
+	public GameObject prefabRoomButton;
+	public List<GameObject> listRoomButton;
 	
-		void Awake () {
-			listRoomButton = new List<GameObject> ();
-		}
+	void Awake () {
+		listRoomButton = new List<GameObject> ();
+	}
 
-		// Use this for initialization
-		void Start () {
-			getRooms ();	
-		}
+	// Use this for initialization
+	void Start () {
+		getRooms ();	
+	}
 	
-		// Update is called once per frame
-		void Update () {
+	// Update is called once per frame
+	void Update () {
 	
-		}
+	}
 
-		public void getRooms ()
-		{
-			RoomInfo[] rooms = lobby.GetRoomList();
-			foreach (RoomInfo room in rooms) {
-				GameObject newButton = Instantiate<GameObject>(prefabRoomButton);
-				newButton.transform.parent = this.transform;
-				RoomButton infoButton = newButton.GetComponent<RoomButton>();
-				infoButton.text.text = room.Name + "(" + room.PlayerCount +"/" + room.MaxPlayers + ")";
-				infoButton.roomName = room.Name;
-				listRoomButton.Add(newButton);
-			}
+	public void getRooms ()
+	{
+		RoomInfo[] rooms = lobby.GetRoomList();
+		print("Rooms available");
+		print (rooms.Length);
+		foreach (RoomInfo room in rooms) {
+			print(room.ToString() + "name : "+ room.Name);
+			GameObject newButton = Instantiate<GameObject>(prefabRoomButton);
+			newButton.transform.parent = this.transform;
+			RoomButton infoButton = newButton.GetComponent<RoomButton>();
+			infoButton.text.text = room.Name + "(" + room.PlayerCount +"/" + room.MaxPlayers + ")";
+			infoButton.roomName = room.Name;
+			listRoomButton.Add(newButton);
 		}
 	}
+}

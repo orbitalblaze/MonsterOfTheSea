4bf0fac747273b9a080fd49988020061b2dc2cbc
Luxgnite <ewan.dubois@live.fr>
2017-06-22 18:08:02 +0200

    WIP

---
+++ Assets/Scripts/System/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
